6c2e6c0fff1b1dcaf32285918efb998caa2a62e1
rwrigg-hass <47410131+rwrigg-hass@users.noreply.github.com>
2019-04-26 01:52:35 -0500

    Update suricata-cfg.sh
    Added vars section update

--- suricata-cfg.sh
+++ suricata-cfg.sh
@@ -2,11 +2,58 @@
 
 
 nsm_dir=/etc/nsm
+suricata_vars_file=/tmp/suricata_vars.yaml
 
+function usage {
+echo "Usage:
+$suricata_vars_file must exist containing the vars section of suricata.yaml to by applied"
+echo '        
+### example ###
+vars:
+  # more specific is better for alert accuracy and performance
+  address-groups:
+    HOME_NET: "[10.0.0.0/8]"
+    #HOME_NET: "[192.168.0.0/16]"
+    #HOME_NET: "[10.0.0.0/8]"
+    #HOME_NET: "[172.16.0.0/12]"
+    #HOME_NET: "any"
+
+    #EXTERNAL_NET: "!$HOME_NET"
+    EXTERNAL_NET: "!$HOME_NET"
+
+    HTTP_SERVERS: "$HOME_NET"
+    SMTP_SERVERS: "$HOME_NET"
+    SQL_SERVERS: "$HOME_NET"
+    DNS_SERVERS: "$HOME_NET"
+    TELNET_SERVERS: "$HOME_NET"
+    AIM_SERVERS: "$EXTERNAL_NET"
+    DC_SERVERS: "$HOME_NET"
+    DNP3_SERVER: "$HOME_NET"
+    DNP3_CLIENT: "$HOME_NET"
+    MODBUS_CLIENT: "$HOME_NET"
+    MODBUS_SERVER: "$HOME_NET"
+    ENIP_CLIENT: "$HOME_NET"
+    ENIP_SERVER: "$HOME_NET"
+
+  port-groups:
+    HTTP_PORTS: "80"
+    SHELLCODE_PORTS: "!80"
+    ORACLE_PORTS: 1521
+    SSH_PORTS: 22
+    DNP3_PORTS: 20000
+    MODBUS_PORTS: 502
+    FILE_DATA_PORTS: "[$HTTP_PORTS,110,143]"
+    FTP_PORTS: 21'
+}
 
 function modify_suricata {
     cp $suricata_file ${suricata_file}-`date +"%Y%m%d-%H%S"`
     
+    lead='## Step 1: inform Suricata about your network'
+    tail='## Step 2: select outputs to enable'
+    sed -e "/$lead/,/$tail/{ /$lead/{N;N;p; r $suricata_vars_file
+            }; s/$tail/\n\n##\n$tail/p; d }" -i $suricata_file
+     
     sed -e '/- eve-log:/!b' -e ':a' \
         -e "s/\(enabled:\).*/\1 yes/;t trail" \
         -e 'n;ba' -e ':trail' -e 'n;btrail' -i $suricata_file
@@ -84,6 +131,13 @@ function modify_suricata {
         -e 'n;ba' -e ':trail' -e 'n;btrail' -i $suricata_file
 
 }
+
+
+
+if [[ ! -e $suricata_vars_file ]]
+  usage
+  exit
+fi
 
 while IFS= read -r line ;do
     worker=`echo $line | cut -d' ' -f 1`
